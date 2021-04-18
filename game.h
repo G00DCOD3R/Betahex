9a2cac0af8eb120120f0df7a8da8b5ea81c0555f
G00DCOD3R <82707639+G00DCOD3R@users.noreply.github.com>
2021-04-18 13:44:39 +0200

    Basic files

---
+++ game.h
@@ -0,0 +1,33 @@
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define se second 
+#define all(...) begin(__VA_ARGS__) , end(__VA_ARGS__)
+#define boost {ios_base::sync_with_stdio(false); cin.tie(); cout.tie();}
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef vector <int> vi;
+typedef pair<int,int> PII;
+typedef pair<ll,ll> PLL;
+
+class game
+{
+	public:
+		static const int N = 11;
+		int br[N+1][N+1];
+		int vis[N+1][N+1], nrv = 1, moves_made = 0;
+		stack <PII> mvs;
+		bool my_id, op_id, turn=0;
+		bool good(int x, int y);
+		void __init__();
+		void show();
+		void declare_winner(bool who);
+		int make(int x, int y, bool who);
+		void undo_move();
+		void let_make_move();
+};
